4fb819c531d3258ba26b6c59d2a6423f51ff041b
Nani6238 <pnani6238@gmail.com>
2026-04-14 12:29:17 +0530

    added python basics file

---
+++ python_basics_nani.py
@@ -0,0 +1,17 @@
+#Task-1
+name="Raviteja"
+age=19
+height=165.4
+is_student=True
+print(name,age,height,is_student)
+#Task-2
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+#Task-3
+months=age*12
+days=age*365
+remainder=age%7
+power=age**2
+print("Age in months:", months)
+print("Age in days:", days)
+print("Remaiinder age is divisble by 7:", remainder)
+print("Age raised to the power:", power)
